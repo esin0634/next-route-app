4e9b5d65d8e24b689fe49da1351a39bdcf269060
Esin <nbusra.esin@gmail.com>
2023-06-16 13:13:08 +0300

    added key.id to post elements

--- app/users/[userId]/Post.js
+++ app/users/[userId]/Post.js
@@ -5,7 +5,7 @@ const Post = ({posts}) => {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 px-3 '>
             {posts.map((post) =>(
-                <div className='m-5 p-5 bg-gradient-to-r rounded-2xl from-teal-500 to-purple-600  hover:text-gray-900 active:border '>
+                <div key={post.id} className='m-5 p-5 bg-gradient-to-r rounded-2xl from-teal-500 to-purple-600  hover:text-gray-900 active:border '>
                     <div className='flex-wrap title font-semibold py-5'> 
                         <h3> {post.title} </h3> 
                     </div>
